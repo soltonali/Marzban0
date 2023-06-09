fb159d3de2677ec3bba65cdf18d5a4bc31f15599
Mahdi Shariat <blckht.49@gmail.com>
2023-06-09 14:58:34 +0330

    init

--- .github/easy_setup.sh
+++ .github/easy_setup.sh
@@ -6,25 +6,24 @@ rm -rf "/var/lib/marzban";
 
 mkdir -p /opt/marzban;
 mkdir -p /var/lib/marzban/
-
 cd /opt/marzban;
+
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
 git clone https://github.com/soltonali/Marzban0.git;
-mv ./Marzban0/* ./;
+mv ./Marzban0/* ./ && rm -rf ./Marzban0;
+
 cp ./xray_config.json /var/lib/marzban/xray_config.json
-rm -rf ./Marzban
-rm -rf ./Marzban0
+
 clear;
 echo "marzban downloaded";
 echo "install deps..."
-pip install -r requirements.txt
-clear;
+pip install -r requirements.txt > /dev/null
+
 alembic upgrade head
 sudo ln -s $(pwd)/marzban-cli.py /usr/bin/marzban-cli
 sudo chmod +x /usr/bin/marzban-cli
 marzban-cli completion install
 cp .env.example .env
-#nano  m .env
 cp ./marzban0.service /var/lib/marzban/marzban.service
 systemctl enable /var/lib/marzban/marzban.service
 systemctl start marzban
